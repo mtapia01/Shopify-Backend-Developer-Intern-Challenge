a7d13e12ff863ce29f97648d23a5afee3edd430f
michael <michaeltapia80@yahoo.com>
2022-01-12 18:33:14 -0800

    Feat(index,app,main): Can now display csv files

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
+import csv
+import json
 
+from datetime import date
 app = Flask(__name__, static_folder = 'static', template_folder = 'templates')
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 CORS(app)
@@ -13,6 +16,69 @@ def home():
     return render_template('index.html')
 
 
+@app.route('/newitem')
+def newitem():
+        item = request.args.get("item")
+        currentDate = date.today()
+        newListing = [
+                item,
+                currentDate
+        ]
+        with open('inventory.csv', 'a', newline='\n')as file:
+                writer = csv.writer(file)
+                writer.writerow(newListing)
+        return {"inventoryList": newListing}, 201
+
+@app.route('/cvsDisplay')
+def cvsDisplay():
+        cvsFile = open('inventory.csv', 'r')
+        # print(cvsFile)
+        inventory = cvsFile.readlines()
+        cvsFile.close
+        # print(inventory)
+        
+        for i in range(len(inventory)):
+                inventory[i] = inventory[i].strip()
+
+        cleanFile = []
+
+        for i in range(len(inventory)):
+                row = inventory[i]
+                element = row.split(',')
+                item = element[0]
+                itemDate = element[1]
+                cleanRow = {
+                        'cleanItem': item,
+                        'cleanDate': itemDate
+                }
+                cleanFile.append(cleanRow)
+
+        return {'cleanListing': cleanFile}
+
+@app.route('/listOfItems')
+def listOfItems():
+        item = request.args.get('item')
+        print(item)
+        file = open('itemList.txt', 'a')
+        file.writelines([json.dumps(item), '\n'])
+        file.close()
+
+        return "", 201
+
+@app.route('/getItemList')
+def getItemList():
+        file = open('itemList.txt', 'r')
+        lines = file.readlines()
+
+        items = []
+
+        for i in lines:
+                i = i.strip()
+                itemJSON = json.loads(i)
+                items.append(itemJSON)
+        file.close()
+
+        return {"items": items}
 
 # ===============================================================================
 if __name__ == '__main__':
--- static/main.js
+++ static/main.js
@@ -1,6 +1,53 @@
+let itemID = -1;
 $(document).ready(function () {
   $("#addItemBtn").click(function () {
     let item = $("#itemInput").val();
-    console.log(item);
+    listing = { item: item };
+
+    $.get("/newitem", listing, function (response) {
+      console.log(response["inventoryList"]);
+      let listing = response["inventoryList"];
+      let listingItem = listing[0];
+      let listingDate = listing[1];
+      let line = `<tr><td>${listingItem}</td></tr>`;
+      line = line + `<td>${listingDate}</td>`;
+      $("inventory").append(line);
+    });
+  });
+
+  $.get("/cvsDisplay", {}, function (response) {
+    let itemListings = response["cleanListing"];
+    console.log(itemListings);
+
+    for (let i = 0; i < itemListings.length; i++) {
+      let current = itemListings[i];
+      let fileItem = current["cleanItem"];
+      let fileDate = current["cleanDate"];
+
+      let line = `<tr><td>${fileItem}</td>`;
+      line = line + `<td>${fileDate}</td></tr>`;
+      $("#inventory").append(line);
+    }
+  });
+
+  function getItemList() {
+    $.get("/getItemList", {}, function (response) {
+      let items = response["items"];
+
+      for (let i = itemID + 1; i < items; i++) {
+        let item = items[i]["items"];
+
+        $("#itemList").append(`${item}<br></br>`);
+      }
+    });
+  }
+  getItemList();
+  setInterval(getItemList, 5000);
+  $("#addItemBtn").click(function () {
+    let item = $("#itemInput").val();
+
+    $.get("/listOfItems", item, function (response) {
+      getItemList();
+    });
   });
 });
